6e6885b7b57e82fd81742e1af66c4d6e33f1132b
Sahar <sahar.sadrii.ss@gmail.com>
2019-12-27 16:06:08 +0330

    this class keep's them's attributes

---
+++ C#/PracticeClass/PracticeClass/classThem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeClass
+{
+
+    public class classThem
+    {
+        //variable for form's object's colour
+        public System.Drawing.Color sideMenuBack;
+        public System.Drawing.Color mainText;
+        public System.Drawing.Color unmainText;
+        public System.Drawing.Color background;
+        public System.Drawing.Color row;
+        public System.Drawing.Color borderWarning;
+        public System.Drawing.Color borderOccur;
+        public 
+
+       
+        classThem()
+        {
+            sideMenuBack = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
+            mainText = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(153)))), ((int)(((byte)(251)))));
+            unmainText = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(204)))), ((int)(((byte)(253)))));
+            background = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(249)))), ((int)(((byte)(255)))));
+            row = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            borderWarning = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(102)))), ((int)(((byte)(38)))));
+            borderOccur = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(216)))), ((int)(((byte)(80)))));
+        }
+        public void changeThemToLight()
+        {
+            sideMenuBack = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
+            mainText = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(153)))), ((int)(((byte)(251)))));
+            unmainText = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(204)))), ((int)(((byte)(253)))));
+            background = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(249)))), ((int)(((byte)(255)))));
+            row = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            borderWarning = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(102)))), ((int)(((byte)(38)))));
+            borderOccur = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(216)))), ((int)(((byte)(80)))));
+        }
+        
+    }
+    
+}
