d36ee993b35aaa454d244b4a8e07cbbea7d52b44
Reza ADB <adbrezaooo@gmail.com>
2020-01-06 15:23:01 +0330

    debuged

--- C#/PracticeClass/PracticeClass/MainWindow.xaml.cs
+++ C#/PracticeClass/PracticeClass/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
 namespace PracticeClass {
     /// <summary>
@@ -7,6 +9,11 @@ namespace PracticeClass {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            classBase baseClass = new classBase();
+            classStudent student= baseClass.LoginStudent("961845102", "1");
+            List<ShowClass> temp = student.GetStudentOnGoingClassesList();
+            ShowClass f = temp.First();
+            classPracticeClass pc = new classPracticeClass(f.term, f.year, f.groupNumber);
         }
     }
 }
